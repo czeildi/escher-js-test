e1e0bd3950c21f3e04bfbb88813ad11d36deb9ff
Ildiko Czeller <czeildi@gmail.com>
2019-09-09 21:36:06 +0200

    add logs on js server side

--- server/express.js
+++ server/express.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 
 app.get('/validate_request_js_js', (req, res) => {
+  console.log(req.path, req.headers)
   res.send('request from js to js: ' + req.headers['x-ems-date']);
 });
 
 app.get('/validate_request_py_js', (req, res) => {
+  console.log(req.path, req.headers)
   res.send('request from py to js: ' + req.headers['x-ems-date']);
 });
 
